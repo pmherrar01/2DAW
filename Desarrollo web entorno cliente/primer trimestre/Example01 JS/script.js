13589a2efbc4de9666e27e5ef3a8f9a6de64052d
pmherrar01 <pmherrerar01@educarex.es>
2025-09-15 12:23:18 +0200

    1

--- Desarrollo web entorno cliente/primer trimestre/Example01 JS/script.js
+++ Desarrollo web entorno cliente/primer trimestre/Example01 JS/script.js
@@ -1,10 +1,12 @@
 // Capture data from user
 let name = prompt("Enter your name:");
+sadansdfpksdf;  
 let age = prompt("Enter your age:");
 let color = prompt("Enter your favorite color:");
+let study = prompt("What are you studing?");
 
 // Create message
-let message = "Hello, " + name + "! You are " + age + " years old and your favorite color is " + color + ".";
+let message = "Hello, " + name + "! You are " + age + " years old and your favorite color is " + color + "and you are studying " + study + ".";
 
 // Show in console
 console.log(message);
@@ -15,6 +17,12 @@ if (age > 18) {
   console.log("You are an adult!");
 } else {
   console.log("You are a minor!");
+}
+
+if(study == "programming"){
+  console.log("You are studying " + study)
+}else{
+  console.log("you not are studying programming")
 }
 
 // Change background color
