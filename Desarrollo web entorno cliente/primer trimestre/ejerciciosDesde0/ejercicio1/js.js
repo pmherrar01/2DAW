953fac11005471d2b0bc01e20901e3ba10f45786
pmherrar01 <pmherrerar01@educarex.es>
2025-09-16 18:43:20 +0200

    ejercicio2 terminado

---
+++ Desarrollo web entorno cliente/primer trimestre/ejerciciosDesde0/ejercicio1/js.js
@@ -0,0 +1,9 @@
+let nombre = "pablo";
+const edad = 20;
+let estudio = true;
+
+if (estudio) {
+    console.log(nombre + edad + " si estudio");    
+}
+
+
---
+++ Desarrollo web entorno cliente/primer trimestre/ejerciciosDesde0/ejercicio2/js.js
@@ -0,0 +1,10 @@
+let num = Number(prompt("insert number"));
+let num2 = Number(prompt("insert number"));
+
+function suma(params1, params2) {
+    return params1 + params2
+}
+
+let resultado = suma(num, num2);
+
+console.log(resultado);
