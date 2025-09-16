adbb58d0c5e21da4ce4dd7efb5c8dd3c1a0a032f
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-09-16 13:12:18 +0200

    Task01 Async Vs Defer finish

---
+++ Desarrollo web entorno cliente/primer trimestre/Task01 Async Vs Defer/js1.js
@@ -0,0 +1,3 @@
+console.log("script async started");
+let hola = prompt("escribe");
+console.log("script async end");
---
+++ Desarrollo web entorno cliente/primer trimestre/Task01 Async Vs Defer/js2.js
@@ -0,0 +1,3 @@
+let hello = prompt("hello");
+console.log("script defer started");
+console.log("script defer end");
