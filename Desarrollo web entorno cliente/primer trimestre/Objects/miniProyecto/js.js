f41928f82a3fe0988288d6d7c00445f5c0fb9f89
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-09-26 14:27:01 +0200

    miniProyecto por terminar

--- Desarrollo web entorno cliente/primer trimestre/Objects/miniProyecto/js.js
+++ Desarrollo web entorno cliente/primer trimestre/Objects/miniProyecto/js.js
@@ -1,42 +1,52 @@
 let prenda1 = {
-    tipoDePrenda : "sudadera",
-    precio : 55,
-    fechaSalida : new Date("2025-09-2"),
+    tipoDePrenda: "sudadera",
+    precio: 55,
+    fechaSalida: new Date("2025-09-2"),
     tara : false
 }
 
 let prenda2 = {
-    tipoDePrenda : "camiseta",
-    precio : 30,
-    fechaSalida : new Date("2019-12-20"),
+    tipoDePrenda: "camiseta",
+    precio: 30,
+    fechaSalida: new Date("2019-12-20"),
     tara : true 
 }
 
 let prenda3 = {
-    tipoDePrenda : "gorra",
-    precio : 20,
-    fechaSalida : new Date("2018-03-2"),
+    tipoDePrenda: "gorra",
+    precio: 20,
+    fechaSalida: new Date("2018-03-2"),
     tara : false
 }
 
 let aStock = [];
 
-function añadirPrenda(prenda, aPrendas) {
+function anadirPrenda() {
 
-    aPrendas.push(prenda);
-    
+    aStock.push(prenda1);
+    aStock.push(prenda2);
+    aStock.push(prenda3);
+
+    console.log(aStock)
+
+    let prendaNueva = datosPrenda(document.getElementById("tipodePrenda").value, document.getElementById("precio").value, document.getElementById("fechaSalida").value, document.querySelector('input[name="tara"]:checked').value === "true");
+
+    aStock.push(prendaNueva);
+
+
+
 }
 
 function datosPrenda(tipoPrenda, precio, fechaSalida, tara) {
-    
-    let prenda = {tipoPrenda, precio, fechaSalida, tara}
+
+    let prenda = { tipoPrenda, precio, fechaSalida, tara }
 
     return prenda;
 }
 
 function mostrarStock(aPrendas) {
     for (let i = 0; i < aPrendas.length; i++) {
-        
+
         console.log(aPrendas[i]);
 
     }
@@ -47,13 +57,14 @@ function borrarPrenda(tipoDePrenda, aPrendas) {
     let i = aPrendas.indexOf(tipoDePrenda);
 
     if (i !== tipoDePrenda) {
-        aPrendas.slice(i,1);
+        aPrendas.splice(i, 1);
     }
 
 }
 
-añadirPrenda(prenda1, aStock);
-añadirPrenda(prenda2, aStock);
-añadirPrenda(prenda3, aStock);
+
+mostrarStock(aStock);
+
+anadirPrenda();
 
 mostrarStock(aStock);
