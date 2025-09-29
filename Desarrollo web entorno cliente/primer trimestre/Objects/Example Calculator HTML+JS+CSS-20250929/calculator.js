39ae5d6b6fcc274cac24ba6b707a8f9d99cd6989
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-09-29 12:04:40 +0200

    s

--- Desarrollo web entorno cliente/primer trimestre/Objects/Example Calculator HTML+JS+CSS-20250929/calculator.js
+++ Desarrollo web entorno cliente/primer trimestre/Objects/Example Calculator HTML+JS+CSS-20250929/calculator.js
@@ -1,30 +1,45 @@
+function comprobarNum() {
+  let op1 = document.getElementById("op1").value;
+  let op2 = document.getElementById("op2").value;
+
+  if (isNaN(op1) || isNaN(op2) || op1 === "" || op2 === "") {
+    alert("Please enter valid numbers.");
+    return false;
+  }
+  return true;
+}
+
 
 function calculate() {
-  let op1 = parseFloat(document.getElementById("op1").value);
-  let op2 = parseFloat(document.getElementById("op2").value);
 
-  let operation = document.forms[0].elements["operation"].value;
 
-  let result;
+  if (!comprobarNum()) {
 
-  switch (operation) {
-    case "add":
-      result = op1 + op2; 
-      break;
-    case "subtract":
-      result = op1 - op2; 
-      break;
-    case "multiply":
-      result = op1 * op2; 
-      break;
+    let op1 = parseFloat(document.getElementById("op1").value);
+    let op2 = parseFloat(document.getElementById("op2").value);
+
+    let operation = document.forms[0].elements["operation"].value;
+
+    let result;
+
+    switch (operation) {
+      case "add":
+        result = op1 + op2;
+        break;
+      case "subtract":
+        result = op1 - op2;
+        break;
+      case "multiply":
+        result = op1 * op2;
+        break;
       case "divide":
-      result = op1 / op2;
-      break;
-    default:
-      console.log("error"); 
-      return;
+        result = op1 / op2;
+        break;
+      default:
+        console.log("error");
+        return;
+    }
+
+    console.log("Result:", result);
   }
-
-
-  console.log("Result:", result);
 }
