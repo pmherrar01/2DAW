330adb0169d2615d9d1bbaa82ccfd7c7a399523a
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-09-30 14:26:50 +0200

    ejercicio2 termiando

---
+++ Desarrollo web entorno cliente/primer trimestre/Ejercicios/Task 02 - Student Grades with for...in/js.js
@@ -0,0 +1,60 @@
+let students = [];
+
+function anadirStudient(nameStudent, math, languaje, science) {
+    let student = {
+        name: nameStudent,
+        math: math,
+        languaje: languaje,
+        science: science
+    };
+    return student;
+}
+
+function comprobarString() {
+    let nameStudent = document.getElementById("name").value;
+
+    if(nameStudent.trim() === "") {
+        alert("Por favor, ingresa un nombre");
+    }else{
+        return nameStudent;
+    }
+
+}
+
+function comprobarNum(num) {
+    let nota = document.getElementById(num).value;
+
+    if(isNaN(nota) || nota < 0 || nota > 10) {
+        alert("Please, enter a valid number");
+    }else{
+        return nota;
+    }
+
+}
+
+function calculateAverageGrades(student) {
+    let averageGrades = 0;
+    for (const key in student) {
+        if (key === "name") {
+            continue;
+        }
+        averageGrades += Number(student[key]);
+    }
+
+    return averageGrades / students.length++;
+}
+
+
+function control() {
+    let nameSt = comprobarString();
+    let notaMath = comprobarNum("math");
+    let notaLanguaje = comprobarNum("languaje");
+    let notaScience = comprobarNum("science");
+    
+    let newStudent = anadirStudient(nameSt, notaMath, notaLanguaje, notaScience);
+    students.push(newStudent);
+
+    let averageGrades = calculateAverageGrades(newStudent);
+
+    alert(`The average grade of ${newStudent.name} is ${averageGrades}`);
+}
