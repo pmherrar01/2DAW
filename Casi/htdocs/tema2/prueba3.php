254d0169d76fc1210fcce1f3c7f5a84370542ff3
pmherrar01 <120454320+pmherrar01@users.noreply.github.com>
2025-10-02 10:18:12 +0200

    c

--- Casi/htdocs/tema2/prueba3.php
+++ Casi/htdocs/tema2/prueba3.php
@@ -7,14 +7,26 @@
 </head>
 <body>
     <?php
-        $a = 7;
+       // $a=$b="3.1416";
+       // settype($b, "float"); 
+       // print "\$a vale $a y es de tpo".gettype($a);
+       // print"<br />";
+       // print "\$b vale $b y es de tipo".gettype($b);
 
-        function contador()  {
-            global $a;
-            echo $a;// local
-        }
+       $a = 7;
+       $b = "8";
+       $c= true;
+       $d = "ocho";
+       $e = null;
+       unset($e);
+       $resultado=$a+$b+$c;
+       echo $resultado;
 
-        contador();
+       echo "<BR>¿Es numerico \$a? ".(is_numeric($a)?"SI":"NO" );
+       echo "<BR>¿Es numerico \$d? ".(is_numeric($d)?"SI":"NO" );
+
+       echo "<BR>isset (\$e): ".(isset($e)?"true":"false");
+       echo "<BR>empty (\$e): ".(empty($e)?"true":"false");
     ?>
 </body>
 </html>
