2310970ccb7498068b6fc48e0c0c9ca5a9ab81c8
pmherrar01 <pmherrerar01@educarex.es>
2025-10-09 19:29:00 +0200

    ejercicio4 por teminar
    faltan mejorar las validaciones

--- Desarrollo web entorno cliente/primer trimestre/Ejercicios/Task 03 Your Personalized Collection_ PHASE1/js.js
+++ Desarrollo web entorno cliente/primer trimestre/Ejercicios/Task 03 Your Personalized Collection_ PHASE1/js.js
@@ -1,5 +1,12 @@
 //Array para guardar las prendas
 let aStock = [];
+let cont = 0; 
+/*
+    Utilizo esta variable count para contar cuetnas prendas añado de una vez,
+    me refiero de de una vex se presiona 2 veces el boton "enviar" pues se añaden 2 prendas de una vez entonces contaria 2,
+    a parte en el metodo mostrar voy poniendo cuantas prendas tiene el array,
+    por que no se muy bien a que te refieres con que añadamos el contador que muestre cuantos elementos se han añadido al array
+*/ 
 //funcion para añadir una prenda al array
 function anadirPrenda() {
     console.log(aStock)
@@ -12,14 +19,12 @@ function anadirPrenda() {
             document.getElementById("tipodePrenda").value,
             parseFloat(document.getElementById("precio").value),
             document.getElementById("fechaSalida").value,
-            document.querySelector('input[name="tara"]:checked').value === "true"
+            document.querySelector('input[name="tara"]:checked').value === "true" ? true : false 
         );
     }
     aStock.push(prendaNueva);
 
-    mostrarStock(aStock);
-
-
+    cont++;
 
 }
 
@@ -45,11 +50,24 @@ function datosPrenda(tipoPrenda, precio, fechaSalida, tara) {
     return prenda;
 }
 
-//funcion para mostrar el array que lo uso para comprobar que se añaden bien al array
-function mostrarStock(aPrendas) {
-    for (let i = 0; i < aPrendas.length; i++) {
+//funcion para odernar el array por precio
+function ordenarPrecio() {
+    aStock.sort((a, b) => a.precio - b.precio);
 
-        console.log(aPrendas[i]);
+}
 
+//funcion para mostrar
+function mostrarStock() {
+
+    ordenarPrecio();
+
+    alert(`Mostarndo el stock ordenado por precio`);
+
+    for (let i = 0; i < aStock.length; i++) {
+        alert(`Prenda ${i + 1}:   ${aStock[i].tipoPrenda} \nPrecio: ${aStock[i].precio} \nFecha de salida: ${aStock[i].fechaSalida} \n¿Tiene tara?: ${aStock[i].tara}`);
     }
+
+    alert(`Se han añadido ${cont} prendas al stock`);
+    cont = 0;
+    
 }
