c235a3b643eb9175100485bf45b99a330d697870
pmherrar01 <pmherrerar01@educarex.es>
2025-10-14 13:28:14 +0200

    ejercicio4 por temrinar
    falta corregir la validacion de que no se añada un objeto igual

--- Desarrollo web entorno cliente/primer trimestre/Ejercicios/Task 04 Your Personalized Collection_PHASE2/js.js
+++ Desarrollo web entorno cliente/primer trimestre/Ejercicios/Task 04 Your Personalized Collection_PHASE2/js.js
@@ -1,14 +1,15 @@
 //Array para guardar las prendas
 let aStock = [];
-let cont = 0; 
+//variable cont con la que cuento cuantas orendas añado
+let cont = 0;
 /*
     Utilizo esta variable count para contar cuetnas prendas añado de una vez,
     me refiero de de una vex se presiona 2 veces el boton "enviar" pues se añaden 2 prendas de una vez entonces contaria 2,
     a parte en el metodo mostrar voy poniendo cuantas prendas tiene el array,
     por que no se muy bien a que te refieres con que añadamos el contador que muestre cuantos elementos se han añadido al array
-*/ 
+*/
 //funcion para añadir una prenda al array
-function anadirPrenda() { 
+function anadirPrenda() {
     console.log(aStock)
     let prendaNueva;
 
@@ -18,14 +19,22 @@ function anadirPrenda() {
         prendaNueva = datosPrenda(
             document.getElementById("tipoPrenda").value,
             document.getElementById("descripcion").value,
-            parseFloat(document.getElementById("precio").value),
+            document.getElementById("precio").value,
             document.getElementById("fechaSalida").value,
-            document.querySelector('input[name="tara"]:checked').value === "true" ? true : false 
+            document.querySelector('input[name="tara"]:checked').value === "true" ? true : false
         );
     }
-    aStock.push(prendaNueva);
 
-    cont++;
+    if (prendaExiste(prendaNueva)) {
+        alert("La prenda ya existe en el stock");
+    }else{
+        aStock.push(prendaNueva);
+        cont++;
+    }
+
+    
+
+
 
 }
 
@@ -37,7 +46,7 @@ function validarpPrenda() {
     let fechaSalida = document.getElementById("fechaSalida").value;
     let tara = document.querySelector('input[name="tara"]:checked');
 
-    if (tipoPrenda === "" || descripcion === "" || precio === "" || fechaSalida === "" || tara === null) {
+    if (tipoPrenda === ""  || descripcion === "" || precio === "" || fechaSalida === "" || tara === null) {
         return false;
     }
 
@@ -45,9 +54,17 @@ function validarpPrenda() {
 
 }
 //funcion para crear un objeto prenda que lo retorna
-function datosPrenda(tipoPrenda,descripcion, precio, fechaSalida, tara) {
+function datosPrenda(tipoPrenda, descripcion, precio, fechaSalida, tara) {
 
-    let prenda = { tipoPrenda, descripcion, precio, fechaSalida, tara }
+    let prenda = {
+        tipoPrenda, 
+        detalles: [
+            descripcion,
+            precio,
+            fechaSalida,
+            tara
+        ] 
+ }
 
     return prenda;
 }
@@ -58,6 +75,18 @@ function ordenarPrecio() {
 
 }
 
+//funcion para comprobar si una prenda ya existe en el array
+function prendaExiste(prendaNueva) {
+    for (const prenda in aStock) {
+        if(prenda.tipoPrenda === prendaNueva.tipoPrenda && prenda.detalles[0] === prendaNueva.detalles[0] && prenda.detalles[1] === prendaNueva.detalles[1] && prenda.detalles[2] === prendaNueva.detalles[2] && prenda.detalles[3] === prendaNueva.detalles[3]){
+            return true;
+        } 
+    }    
+
+    return false;
+    
+}
+
 //funcion para mostrar
 function mostrarStock() {
 
@@ -65,11 +94,13 @@ function mostrarStock() {
 
     alert(`Mostarndo el stock ordenado por precio`);
 
-    for (let i = 0; i < aStock.length; i++) {
-        alert(`Prenda ${i + 1}:   ${aStock[i].tipoPrenda} \nPrecio: ${aStock[i].precio} \nFecha de salida: ${aStock[i].fechaSalida} \n¿Tiene tara?: ${aStock[i].tara}`);
-    }
+   for (const tipodePrenda in aStock) {
+    alert(`Prenda ${parseInt(tipodePrenda) + 1}:   ${aStock[tipodePrenda].tipoPrenda} \n Descipcion: ${aStock[tipodePrenda].detalles[0]} \nPrecio: ${aStock[tipodePrenda].detalles[1]} \nFecha de salida: ${aStock[tipodePrenda].detalles[2]} \n¿Tiene tara?: ${aStock[tipodePrenda].detalles[3]}`);
+        
+    
+   }
 
     alert(`Se han añadido ${cont} prendas al stock`);
     cont = 0;
-    
+
 }
