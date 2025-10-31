bf522b400ce622a6c2f75b47c54e2af457b65661
Pablo Manuel Herrera Rodríguez <120454320+pmherrar01@users.noreply.github.com>
2025-10-31 11:04:45 +0100

    asdf

---
+++ Desarrollo web entorno cliente/primer trimestre/arrowFuncionAnonima.js
@@ -0,0 +1,14 @@
+// Anonymous function assigned to a variable
+var myFunction = function() {
+  console.log("This is an example of an anonymous function.");
+};
+
+// Invoking the anonymous function
+myFunction();
+
+// Basic arrow function
+const add = (a, b) => {
+  return a + b;
+};
+
+const square = x => x * x;
