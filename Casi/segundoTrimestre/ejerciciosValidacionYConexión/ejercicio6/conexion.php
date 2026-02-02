97ee1fd0ef5e2c896fa8ce2c19e51a4d6ea982d2
Pablo Manuel Herrera Rodríguez <120454320+pmherrar01@users.noreply.github.com>
2026-02-02 09:11:57 +0100

    ejercicio 6 por terminar

--- Casi/segundoTrimestre/ejerciciosValidacionYConexión/ejercicio6/conexion.php
+++ Casi/segundoTrimestre/ejerciciosValidacionYConexión/ejercicio6/conexion.php
@@ -5,16 +5,15 @@ $dbName = "agencia_vuelos";
 $user = "root";
 $password = "";
 
-$dns = "mysql:host=$host;dbname=$dbName;charset=uft8mb4";
+$dns = "mysql:host=$host;dbaname=$dbName;charset=utf8mb4";
 
 try {
 
     $conexion = new PDO($dns, $user, $password);
     $conexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo $e-> getMessage();
 }
 
 ?>
