fff52feba5dd70f93d6275464799996b9b8ab47e
Pablo Manuel Herrera Rodríguez <120454320+pmherrar01@users.noreply.github.com>
2026-02-09 14:30:04 +0100

    ejercicio 2 poo por terminar
    login a medias

--- Casi/segundoTrimestre/POO/ejercicio2/ejercicio1.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Usuario{
-    private $conexionDataBase;
-}
-
-?>
--- Casi/segundoTrimestre/POO/ejercicio2/test_poo.php
+++ Casi/segundoTrimestre/POO/ejercicio2/test_poo.php
@@ -1,13 +1,23 @@
 <?php
 
 require_once "dataBase.php";
+require_once "user.php";
 
 $dataBase = new Database();
 
 $conexion = $dataBase->conectar();
 
 if($conexion){
-    echo "conexion exitosa";
+    $user = new Usuario($conexion);
+
+    $user->setUserName("Usuario POO2");
+    $user->setPassword("12345");
+    if($user->registrar()){
+        echo "Insertado corectamente";
+    }else{
+        echo "Error no se a podido insertar";
+    }
+
 }else{
     echo "conexion fallida";
 }
